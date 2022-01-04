c39d015f67b0a3f378f906dba86ff56be343ff30
Gregor Zunic <gregor.ninjaboy@gmail.com>
2022-01-04 16:05:13 +0100

    format

--- contracts/ERC20CVotes.sol
+++ contracts/ERC20CVotes.sol
@@ -23,7 +23,7 @@ abstract contract ERC20CVotes is AccessControl, ERC20Permit, ERC20Votes {
 
   address[] public delegateAddresses;
   uint256 public minHoldingBlocks = 100_000;
-  uint256 constant internal   POINTSMULTIPLIER = 2**128;  // optimization, see https://github.com/ethereum/EIPs/issues/1726#issuecomment-472352728
+  uint256 internal constant POINTSMULTIPLIER = 2**128; // optimization, see https://github.com/ethereum/EIPs/issues/1726#issuecomment-472352728
 
   function changeMinHoldingBlocks(uint256 _minHoldingBlocks)
     public
@@ -33,29 +33,33 @@ abstract contract ERC20CVotes is AccessControl, ERC20Permit, ERC20Votes {
     minHoldingBlocks = _minHoldingBlocks;
   }
 
-  function subscribeDividends(address _addr) public virtual{
+  function subscribeDividends(address _addr) public virtual {
     require(delegates(_addr) == address(0), "Already subscribed"); // check if already subscribed
     delegateAddresses.push(_addr);
-    delegate(_addr);  // ?
+    delegate(_addr); // ?
   }
-  
 
   /**
-  * return how much dividend should one get per his minHoldingValue. EXagerated for factor POINTSMULTIPLIER!
-  *   In each distribution, there is a small amount of funds which does not get distributed,
-	*     which is `(msg.dividendAmount * POINTSMULTIPLIER) % totalValue()`.
-  */
-  function dividendsPerHoldingValue(uint256 dividendAmount) public view onlyRole(RoleNames.WIZARD) returns (uint256){
+   * return how much dividend should one get per his minHoldingValue. EXagerated for factor POINTSMULTIPLIER!
+   *   In each distribution, there is a small amount of funds which does not get distributed,
+   *     which is `(msg.dividendAmount * POINTSMULTIPLIER) % totalValue()`.
+   */
+  function dividendsPerHoldingValue(uint256 dividendAmount)
+    public
+    view
+    onlyRole(RoleNames.WIZARD)
+    returns (uint256)
+  {
     uint256 arrayLength = delegateAddresses.length;
     uint256 totalHoldingPower = 0; // totalValue auto init to 0
     uint256 pointsPerShare;
 
-    for (uint256 i=0; i<arrayLength; i++) {
+    for (uint256 i = 0; i < arrayLength; i++) {
       totalHoldingPower += minHoldingValue(delegateAddresses[i]);
-      }
+    }
     require(totalHoldingPower > 0, "TTOTAL HOLDING POWER IS ZERO");
     require(dividendAmount > 0, "NO DIVIDENDS TO DISTRIBUTE");
-    
+
     pointsPerShare = dividendAmount.mul(POINTSMULTIPLIER) / totalHoldingPower;
     return pointsPerShare;
   }
@@ -86,11 +90,7 @@ abstract contract ERC20CVotes is AccessControl, ERC20Permit, ERC20Votes {
   /**
    * Return min share of user in last minHoldingBlocks
    */
-  function minHoldingValue(address _addr)
-    public
-    view
-    returns (uint256)
-  {
+  function minHoldingValue(address _addr) public view returns (uint256) {
     require(numCheckpoints(_addr) > 0, "no checkpoints");
     uint256 startBlock = int256(block.number) - int256(minHoldingBlocks) > 0
       ? block.number - minHoldingBlocks
