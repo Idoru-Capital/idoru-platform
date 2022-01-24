464377a7b04f9b67d6cc182c960b0e93a2420042
Gregor Zunic <gregor.ninjaboy@gmail.com>
2022-01-24 17:02:32 +0100

    struggling with swap test

---
+++ test/constants.ts
@@ -0,0 +1,13 @@
+export const USDC = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48";
+export const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+export const UNISWAP_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+export const routerABI = [
+  "function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)",
+];
+export const tokenABI = [
+  "function approve(address spender, uint amount) public returns(bool)",
+  "function balanceOf(address who) public view returns(uint)",
+  "function transfer(address to, uint amount) public returns(bool)",
+  "function transferFrom(address from, address to, uint amount) public returns(bool)",
+];
--- test/platform.test.ts
+++ test/platform.test.ts
@@ -1,6 +1,9 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+// import { expect } from "chai";
+import { ethers, network } from "hardhat";
+import { Contract } from "ethers";
+
+import * as uniswap from "@uniswap/v2-sdk";
 
 // import { Idoru } from "../typechain/index";
 import {
@@ -8,6 +11,65 @@ import {
   Idoru__factory,
   RoleNames,
   RoleNames__factory,
-  ERC20Bank,
-  ERC20Bank__factory,
+  // ERC20Bank,
+  // ERC20Bank__factory,
+  // IdoruMinter,
+  // IdoruMinter__factory,
 } from "../typechain";
+
+import { routerABI, tokenABI, UNISWAP_ROUTER, USDC, WETH } from "./constants";
+
+describe.only("Platform", function () {
+  let token: Idoru;
+  let owner: SignerWithAddress;
+  let addr1: SignerWithAddress;
+  let addr2: SignerWithAddress;
+  let addr3: SignerWithAddress;
+  let addr4: SignerWithAddress;
+
+  let uniswap_router_v2: Contract;
+  let weth: Contract;
+  let usdc: Contract;
+
+  let ROLES_NAMES: RoleNames;
+
+  beforeEach(async function () {
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+
+    const tokenFactory = new Idoru__factory(owner);
+    token = await tokenFactory.deploy();
+    await token.deployed();
+
+    const constantsFactory = new RoleNames__factory(owner);
+    ROLES_NAMES = await constantsFactory.deploy();
+    await ROLES_NAMES.deployed();
+
+    uniswap_router_v2 = new ethers.Contract(UNISWAP_ROUTER, routerABI, owner);
+
+    weth = new ethers.Contract(WETH, tokenABI, owner);
+    usdc = new ethers.Contract(USDC, tokenABI, owner);
+  });
+
+  it("Test Network", async function () {
+    await network.provider.send("hardhat_setBalance", [
+      owner.address,
+      ethers.BigNumber.from(10).pow(24).toHexString(),
+    ]);
+
+    console.log(await ethers.provider.getBalance(owner.address));
+
+    await weth.approve(UNISWAP_ROUTER, ethers.BigNumber.from(10).pow(18));
+
+    console.log(await weth.balanceOf(owner.address));
+
+    await uniswap_router_v2.swapETHForExactTokens(
+      0,
+      [WETH, USDC],
+      owner.address,
+      1_000_000_000_000,
+      {
+        value: 1,
+      }
+    );
+  });
+});
